4d4165de74cfddfc5fdcfee7872b9ca1f8b74080
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-14 04:23:26 +0530

    api updated

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const { generateClient, analyze } = require('../lib/sentimental_analysis')
+const { getEmotions } = require('../lib/emotion_analysis')
 const { RecogniseAudioEmotion } = require('../lib/sentiment_emotion_analysis')
 const client = generateClient('./keys/gcp.json')
 
 const Router = express.Router()
 
 Router.post('/text', async (req, res) => {
+    if (!req.body.text) return res.status(400).json({
+        error: "Bad Request"
+    })
+
+    const text = req.body.text
+
     try {
-        const result = await analyze(client, req.body.text)
-        res.json(result)
+        const sentiments = await analyze(client, text)
+        const emotions = await getEmotions(text)
+        res.json({
+            sentiments,
+            emotions
+        })
     } catch (err) {
         res.status(500).json(err)
     }
